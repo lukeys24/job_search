63775cc71d9f5c1d30fce88225677e6f0487b89a
Luke Seo <lukeyseo@gmail.com>
2020-03-25 00:26:21 -0700

    Clean up

--- client/src/components/Job.js
+++ client/src/components/Job.js
@@ -1,6 +1,4 @@
 import React from "react";
-import Card from "@material-ui/core/Card";
-
 import { Button } from "@material-ui/core";
 import { Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
